47633097fdd4cec7ca72e403c0ea321d22afab32
Paques Pierre <pierre.paques01@gmail.com>
2013-02-21 01:29:58 +0100

    First Commit : rails mysql + git ignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyHome::Application.config.secret_token = '517a30af24cea770166ed33d6090a265df9ae02c230e3d158154ac5bc302400718c3bae6613241a64431b6d75b0e613d9a5fd9bb0c2d4360d7992a538c6ae20d'
