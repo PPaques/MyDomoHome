152e816335cb5bafbc2850bf9fcc92bb22c51c90
maxlf <maximilien.lf@gmail.com>
2013-03-28 11:02:08 +0100

    Descendent sort for history

--- app/models/opening_measure.rb
+++ app/models/opening_measure.rb
@@ -1,6 +1,8 @@
 class OpeningMeasure < ActiveRecord::Base
-  attr_accessible :opened 
+  attr_accessible :opened
 
   belongs_to :opening, inverse_of: :opening_measures
+
+  default_scope :order => "created_at DESC"
 
 end
