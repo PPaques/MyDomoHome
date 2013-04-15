7d5ec1e7adc0ac89fa26c6049c0bb40b23aeb30c
Lombart <cec.lombart@gmail.com>
2013-04-15 14:36:24 +0200

    euh...

--- app/controllers/accueil_controller.rb
+++ app/controllers/accueil_controller.rb
@@ -1,9 +1,8 @@
 class AccueilController < ApplicationController
-  # GET /accueil
   def index
     @home = Home.first(:include => :rooms)
     respond_to do |format|
-      format.html # index.html.erb
+      format.html
     end
   end
 end 
