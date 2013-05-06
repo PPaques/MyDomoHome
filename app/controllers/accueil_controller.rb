6d59c048b97b29ea0e5031832d45320ee6f2285c
Sébastien Matton <seb.matton@gmail.com>
2013-05-06 13:07:27 +0200

    momaj interface

--- app/controllers/accueil_controller.rb
+++ app/controllers/accueil_controller.rb
@@ -2,6 +2,7 @@
 class AccueilController < ApplicationController
   def index
     @home = Home.first(:include => :rooms)
+    @colors = ["#006CFF", "#EE3C19", "#C8F000", "#FF9900"]
     respond_to do |format|
       format.html
     end
