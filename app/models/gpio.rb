97642b1c60750feea39ba623697fa32ef033bf27
Paques Pierre <pierre.paques01@gmail.com>
2013-05-14 23:19:36 +0200

    adding copyright notice for GPIO class

--- app/models/gpio.rb
+++ app/models/gpio.rb
@@ -1,7 +1,9 @@
-# -*- encoding : utf-8 -*-
-class Gpio 
+# This class was extracted from :
+# https://github.com/jwhitehorn/pi_piper
+# Copyright (c) 2013, Jason Whitehorn All rights reserved.
+class Gpio
    attr_reader :pin, :last_value, :value, :direction, :invert
-    
+
     #Initializes a new GPIO pin.
     #
     # @param [Hash] options A hash of options
@@ -15,31 +17,31 @@ class Gpio
       @direction = options[:direction]
       @invert = options[:invert]
       @trigger = options[:trigger]
-      
+
       raise "Invalid direction. Options are :in or :out" unless [:in, :out].include? @direction
       raise "Invalid trigger. Options are :rising, :falling, or :both" unless [:rising, :falling, :both].include? @trigger
-     
+
       File.open("/sys/class/gpio/export", "w") { |f| f.write("#{@pin}") }
       File.open(direction_file, "w") { |f| f.write(@direction == :out ? "out" : "in") }
-      
-      read 
+
+      read
     end
-    
+
     # If the pin has been initialized for output this method will set the logic level high.
     def on
       File.open(value_file, 'w') {|f| f.write("1") } if direction == :out
     end
-    
+
     # Tests if the logic level is high.
     def on?
       not off?
     end
-    
+
     # If the pin has been initialized for output this method will set the logic level low.
     def off
       File.open(value_file, 'w') {|f| f.write("0") } if direction == :out
     end
-    
+
     # Tests if the logic level is low.
     def off?
       value == 0
@@ -50,7 +52,7 @@ class Gpio
     def update_value(new_value)
       !new_value || new_value == 0 ? off : on
     end
-    
+
     # Tests if the logic level has changed since the pin was last read.
     def changed?
       last_value != value
@@ -63,7 +65,7 @@ class Gpio
       loop do
         fd.read
         IO.select(nil, nil, [fd], nil)
-        read 
+        read
         if changed?
           next if @trigger == :rising and value == 0
           next if @trigger == :falling and value == 1
@@ -72,14 +74,14 @@ class Gpio
       end
     end
 
-    # Reads the current value from the pin. Without calling this method first, `value`, `last_value` and `changed?` will not be updated. 
+    # Reads the current value from the pin. Without calling this method first, `value`, `last_value` and `changed?` will not be updated.
     # In short, you must call this method if you are curious about the current state of the pin.
-    def read 
+    def read
       @last_value = @value
       val = File.read(value_file).to_i
       @value = invert ? (val ^ 1) : val
     end
-    
+
     private
     def value_file
       "/sys/class/gpio/gpio#{pin}/value"
@@ -92,5 +94,5 @@ class Gpio
     def direction_file
       "/sys/class/gpio/gpio#{pin}/direction"
     end
-  
+
 end
